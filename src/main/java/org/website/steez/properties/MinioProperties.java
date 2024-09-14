a85df724f9857d829c4ff85c011da71a58085930
isswmq <isswmq@gmail.com>
2024-09-14 22:45:14 +0300

    Add Redis caching to ProductServiceImpl methods

--- src/main/java/org/website/steez/properties/MinioProperties.java
+++ src/main/java/org/website/steez/properties/MinioProperties.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "minio")
 public class MinioProperties {
-
     private String bucket;
     private String url;
     private String accessKey;
--- src/main/java/org/website/steez/service/impl/ProductServiceImpl.java
+++ src/main/java/org/website/steez/service/impl/ProductServiceImpl.java
@@ -2,6 +2,9 @@ package org.website.steez.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -24,6 +27,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "ProductService::findById", key = "#id")
     public Optional<Product> findById(Long id) {
         return productRepository.findById(id);
     }
@@ -36,6 +40,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional
+    @Caching(put = {
+            @CachePut(value = "ProductService::findById", key = "#result.id"),
+            @CachePut(value = "ProductService::findBySku", key = "#result.sku")
+    })
     public Product create(ProductCreateEditDto dto) {
         Product product = productCreateEditMapper.toEntity(dto);
         return productRepository.save(product);
@@ -43,6 +51,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "ProductService::findBySku", key = "#sku")
     public Optional<Product> findBySku(String sku) {
         return productRepository.findBySku(sku);
     }
